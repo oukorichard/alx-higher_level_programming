14f34ed559fc0576883a3c3ae2939ef7e19996ca
oukorichard <oukorichard001@gmail.com>
2022-09-25 23:34:44 +0300

    hot fix

---
+++ 0x01-python-if_else_loops_functions/fibo.py
@@ -0,0 +1,15 @@
+#fibonacci number module
+def fin(n): #writes fibonacci serries up to n
+ a, b = 0, 1
+ while a < n:
+  print(a, end='')
+  a, b = b, a+b
+ print()
+
+def fib2(n):
+ result =[]
+ a, b = 0, 1
+ while a < n:
+  result. append(a)
+  a, b = b, a+b
+ return result
