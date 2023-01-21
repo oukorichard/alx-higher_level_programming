4937992393bb02079771d80ae54316b953efb9eb
oukorichard <oukorichard001@gmail.com>
2023-01-21 16:03:53 +0300

    new commits

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-""" This checks if the name main is a global variable"""
+""" returns sum of 1 and 2"""
 
     from add_0 import add
-"""This imports function add form module add_0"""
 
     a = 1
     b = 2
-"""Prints the sum of the two digits"""
     print("{} + {} = {}".format(a, b, add(a, b)))
