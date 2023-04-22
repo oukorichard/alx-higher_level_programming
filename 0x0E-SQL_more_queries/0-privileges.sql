6c0b5abe36942d21258ca2f2b836d81550973c5a
oukorichard <oukorichard001@gmail.com>
2023-04-22 23:35:39 +0300

    new commits

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -2,5 +2,5 @@
 
 -- sql query that shows users privillages
 
-SHOW GRANT FOR 'user_0d_1'@'localhost';
-SHOW GRANT FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
