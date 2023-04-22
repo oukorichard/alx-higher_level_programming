5905afbb4f8c86b431c2600083d6d9bd79383325
oukorichard <oukorichard001@gmail.com>
2023-04-22 23:48:46 +0300

    new commits

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,7 @@
 #!/bin/bash
 
--- SQL query that creates a new user
+-- SQL query that creates a new user and grant all previllages
 
 CREATE USER IF NOT EXIST 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+GRANT ALL PREVILLAGES ON '*' TO 'user_0d_1'@'localhost'
