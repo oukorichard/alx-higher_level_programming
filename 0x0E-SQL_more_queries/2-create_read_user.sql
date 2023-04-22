67bfb892751b200fc208009eae9d1c1f4e5a0bc2
oukorichard <oukorichard001@gmail.com>
2023-04-23 00:12:33 +0300

    new commits

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -6,4 +6,6 @@ CREATE DATABASE hbtn_0d_2;
 
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
 
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost' 
+
 GRANT SELECT ON 'hbtn_0d_2' TO 'user_0d_2'@'localhost' 
